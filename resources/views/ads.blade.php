80d0d6351e8274916ee495c69a5aec75e99cac1d
adilcse <adil.cs.work@gmail.com>
2020-05-12 15:17:05 +0530

    adds added

--- resources/views/ads.blade.php
+++ resources/views/ads.blade.php
@@ -1,8 +1,14 @@
-<script type="text/javascript" src="//goraps.com/mobile_redir.php?section=General&pub=189721&ga=g"></script>
-
-
-<script type="text/javascript" src="//goraps.com/pup.php?section=General&pt=2&pub=189721&ga=g"></script>
-
-<script type="text/javascript" src="//uprimp.com/bnr.php?section=General&pub=189721&format=300x250&ga=g"></script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn-server.cc/p/wl-http.js?pub=189721&ga=g"></script>
+    <script type="text/javascript" src="//uprimp.com/bnr.php?section=General&pub=189721&format=300x250&ga=g"></script>
 <noscript><a href="https://yllix.com/publishers/189721" target="_blank"><img src="//ylx-aff.advertica-cdn.com/pub/300x250.png" style="border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" /></a></noscript>
-Showing ads
+    <title>Ads</title>
+</head>
+<body>
+    Showing ads
+</body>
+</html>
